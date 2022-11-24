c2139a45f6f24161b7afda7de98ebf4d9b1881aa
karthyyy <karthik_raveendranath@yahoo.com>
2022-11-24 10:39:02 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,13 @@
+
+# Python code to demonstrate the working of
+# sum()
+  
+numbers = [1,2,3,4,5,1,4,5]
+ 
+# start parameter is not provided
+Sum = sum(numbers)
+print(Sum)
+ 
+# start = 10
+Sum = sum(numbers, 10)
+print(Sum)
